0e1cbbff4d4cdd618cc6af0cc3189ab7bceb8df6
Paul RIBAULT <pribault@e3r5p18.42.fr>
2018-01-13 14:27:44 +0100

    adding mach-o includes

--- include/ft_nm.h
+++ include/ft_nm.h
@@ -6,13 +6,16 @@
 /*   By: pribault <pribault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/13 11:41:36 by pribault          #+#    #+#             */
-/*   Updated: 2018/01/13 11:42:07 by pribault         ###   ########.fr       */
+/*   Updated: 2018/01/13 14:27:02 by pribault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_NM_H
 # define FT_NM_H
 
+# include <mach-o/loader.h>
+# include <mach-o/nlist.h>
+# include <mach-o/fat.h>
 # include "libft.h"
 
 #endif
--- include/ft_otool.h
+++ include/ft_otool.h
@@ -6,13 +6,16 @@
 /*   By: pribault <pribault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/13 11:42:40 by pribault          #+#    #+#             */
-/*   Updated: 2018/01/13 11:44:03 by pribault         ###   ########.fr       */
+/*   Updated: 2018/01/13 14:27:10 by pribault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_OTOOL_H
 # define FT_OTOOL_H
 
+# include <mach-o/loader.h>
+# include <mach-o/nlist.h>
+# include <mach-o/fat.h>
 # include "libft.h"
 
 #endif
